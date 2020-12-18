aa85b235b09af13293b23bef7a22928e71a24d2d
Rishabh Jha <rishabhjha2203@gmail.com>
2020-12-18 20:42:26 +0530

    Using VGG16 {CNN Layers(params fixed) || fc1(25088->4096),fc2(4096->512),fc3(512->2)

--- model.py
+++ model.py
@@ -1,12 +1,15 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 ## Defining the NN architecture
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        # defining the layers used in this model
 
-    def forward(self, x):
-        # defining the structure of the model
-        return x
+# Load the pretrained VGG16 architechture from pytorch
+vgg16 = models.vgg16(pretrained=True)
+
+penultimate_layer = nn.Linear(4096, 512)
+last_layer = nn.Linear(512, 2)
+
+#Changing layers
+vgg16.classifier[3] = penultimate_layer
+vgg16.classifier[6] = last_layer
