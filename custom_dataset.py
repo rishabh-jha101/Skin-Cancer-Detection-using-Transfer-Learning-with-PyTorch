e59c2e14bb662e17c9ac91f04cafa536f88e7cb3
Rishabh Jha <rishabhjha2203@gmail.com>
2020-12-18 20:33:48 +0530

    Added a Custom Dataset Module

---
+++ custom_dataset.py
@@ -0,0 +1,28 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+import torch
+
+class SkinCancerDataset(Dataset):
+    def __init__(self, data_dict, data_dir, transforms = None):
+
+        self.info = pd.DataFrame(data_dict)
+        self.root = data_dir
+        self.transforms = transforms
+    def __len__(self):
+
+        return len(self.info)
+
+    def __getitem__(self, idx):
+
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img_name = os.path.join(self.root,
+                                self.info.iloc[idx, 0])+".jpg"
+        image = Image.open(img_name).convert("RGB")
+
+
+        return (img_as_tensor, torch.tensor(int(label)))
