fac213f458b8311cc4b6152e5ada131b6816fe8e
Rishabh Jha <rishabhjha2203@gmail.com>
2020-12-18 20:38:44 +0530

    Split the data 70/20/10 and completed data preparation

--- prepare.py
+++ prepare.py
@@ -1,5 +1,7 @@
 # from torchvision import datasets
 import numpy as np
+import json
+from custom_dataset import SkinCancerDataset
 import torch.utils.data
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
@@ -11,31 +13,42 @@ batch_size = 20
 # percentage of training set to use as validation
 valid_size = 0.2
 
-# convert data to a normalized torch.FloatTensor
+test_size = 0.1
+
+# convert data to a normalized torch.FloatTensor and Resize
 transform = transforms.Compose([
+    transforms.Resize(size=224),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225]) # Imagenet standards
     ])
+#data directory
+data_dir = "C:/Users/RJ/Desktop/Deep Learning/Projects/skin_cancer_transfer/data/"
+with open('labels.json') as f:
+    targets = json.load(f)
 
 # choose the training and test datasets with transforms
-train_data = np.array([1,2,3,4])
-test_data = np.array([1,2])
+train_data = SkinCancerDataset(targets.items(), data_dir, transform)
 
 # obtain training indices that will be used for validation
 num_train = len(train_data)
 indices = list(range(num_train))
 np.random.shuffle(indices)
-split = int(np.floor(valid_size * num_train))
-train_idx, valid_idx = indices[split:], indices[:split]
+
+#Slicing Train/Valid/Test in 70/20/10 ratio
+split1 = int(np.floor(test_size * num_train))
+split2 = int(np.floor(valid_size * num_train))
+test_idx, valid_idx, train_idx = indices[:split1], indices[split1:split2], indices[split2:]
 
 # define samplers for obtaining training and validation batches
 train_sampler = SubsetRandomSampler(train_idx)
 valid_sampler = SubsetRandomSampler(valid_idx)
+test_sampler = SubsetRandomSampler(test_idx)
 
 # prepare data loaders (combine dataset and sampler)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
     sampler=train_sampler, num_workers=num_workers)
 valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
     sampler=valid_sampler, num_workers=num_workers)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
-    num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    sampler=test_sampler, num_workers=num_workers)
